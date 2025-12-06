0ddf3d727c6784e904ecc97da3658587a5a9c77b
berylatieno <berylatieno30@gmail.com>
2025-12-06 12:11:29 +0300

    id generation utility function

---
+++ internal/utils/id-generator.go
@@ -0,0 +1,13 @@
+package utils
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+// GenerateID generates a random hex ID
+func GenerateID() string {
+	b := make([]byte, 16)
+	rand.Read(b)
+	return hex.EncodeToString(b)
+}
