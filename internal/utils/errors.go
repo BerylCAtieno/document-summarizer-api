0e77bb6854ddd8fc5ccc1e6b76482c7ce83c8a2b
berylatieno <berylatieno30@gmail.com>
2025-12-06 12:12:11 +0300

    errors

---
+++ internal/utils/errors.go
@@ -0,0 +1,40 @@
+package utils
+
+import "net/http"
+
+type AppError struct {
+	StatusCode int
+	Message    string
+}
+
+func (e *AppError) Error() string {
+	return e.Message
+}
+
+func NewBadRequestError(message string) *AppError {
+	return &AppError{
+		StatusCode: http.StatusBadRequest,
+		Message:    message,
+	}
+}
+
+func NewNotFoundError(message string) *AppError {
+	return &AppError{
+		StatusCode: http.StatusNotFound,
+		Message:    message,
+	}
+}
+
+func NewInternalError(message string) *AppError {
+	return &AppError{
+		StatusCode: http.StatusInternalServerError,
+		Message:    message,
+	}
+}
+
+func NewUnauthorizedError(message string) *AppError {
+	return &AppError{
+		StatusCode: http.StatusUnauthorized,
+		Message:    message,
+	}
+}
